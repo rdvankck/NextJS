c6b1b7fb5d786d157352b6b369dada067c277628
rdvankck <kck.rdvan@gmail.com>
2026-03-31 20:18:24 +0300

    feat: add admin page (protected by middleware)

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>Admin Panel</h1>
+        <p>Only Admins can login here.</p>
+      </div>
+    );
+  }
